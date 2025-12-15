5a2a0656ae724a27cd86acde11aee73d124efb88
wouter-codes <wouter.klinkenberg@outlook.com>
2025-12-15 16:44:23 +0000

    Add code to calculate two random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,11 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-function runGame() {}
+function runGame() {
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+}
 
 function checkAnswer() {}
 
