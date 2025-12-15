105e115a8c538529d70b58ee61d84180cfe74277
wouter-codes <wouter.klinkenberg@outlook.com>
2025-12-15 20:13:55 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,8 +30,8 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
         // } else if (gameType === "subtract") {
         //     displaySubtractQuestion(num1, num2);
-        // } else if (gameType === "multiply") {
-        //     displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
         // } else if (gameType === "division") {
         //     displayDivisionQuestion(num1, num2);
     } else {
@@ -74,6 +74,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -104,6 +106,10 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion() {}
 
-function displayMultiplyQuestion() {}
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
+}
 
 function displayDivisionQuestion() {}
